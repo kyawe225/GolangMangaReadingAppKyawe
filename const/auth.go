0704e2cabb2d07bb20f552968e7dacb14376188a
kyaw zin htet <kyawe225@gmail.com>
2024-07-24 09:19:49 +0700

    changes to const

--- const/auth.go
+++ const/auth.go
@@ -4,4 +4,5 @@ const (
 	AuthorizationHeaderKey  = "authorization"
 	AuthorizationTypeBearer = "bearer"
 	AuthorizationPayloadKey = "authorization_payload"
+	SecrectSignedKey        = "KyaweTIThrundra2046REwads"
 )
