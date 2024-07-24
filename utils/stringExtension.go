a9051dee67eab21ac7c4b04dfa23e57c416ab986
kyaw zin htet <kyawe225@gmail.com>
2024-07-24 09:10:57 +0700

    added common function to tuils

--- utils/stringExtension.go
+++ utils/stringExtension.go
@@ -1,5 +1,20 @@
 package utils
 
+import (
+	"realPj/mangaReadingApp/modules/shared/dtos"
+
+	"github.com/gin-gonic/gin"
+)
+
 func IsEmpty(s *string) bool {
 	return (*s) == ""
+}
+
+func ExtractLoggedUserId(context *gin.Context) string {
+	var userId string
+	value, ok := context.Get("user")
+	if ok {
+		userId = value.(dtos.RegisterDto).Id
+	}
+	return userId
 }
