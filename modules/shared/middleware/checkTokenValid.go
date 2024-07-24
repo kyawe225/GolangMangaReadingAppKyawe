1755d50ea455983e0cce85ba9cd736946db94085
kyaw zin htet <kyawe225@gmail.com>
2024-07-24 09:10:08 +0700

    add user login check

--- modules/shared/middleware/checkTokenValid.go
+++ modules/shared/middleware/checkTokenValid.go
@@ -11,6 +11,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CheckLoginAdmin(context *gin.Context) {
+	token := context.GetHeader(consts.AuthorizationHeaderKey)
+
+	if utils.IsEmpty(&token) {
+		log.Println("user does not logged in")
+		context.AbortWithStatusJSON(http.StatusUnauthorized, dtos.NewResponseDto("NG", "Not Logged Token", "Not Logged In"))
+		return
+	}
+
+	token = strings.Split(token, consts.AuthorizationTypeBearer)[1]
+	token = strings.Trim(token, " ")
+	dto, err := utils.ValidateToken(token)
+
+	if err != nil {
+		log.Println(err)
+		context.AbortWithStatusJSON(http.StatusUnauthorized, dtos.NewResponseDto("NG", "Please Login Again", "Please Login Again"))
+		return
+	}
+
+	if dto.Role == "admin" {
+		context.AbortWithStatusJSON(http.StatusForbidden, dtos.NewResponseDto("NG", "Forbidden Access", "Please Login With Another Account"))
+		return
+	}
+
+	context.Set("user", dto)
+
+	context.Next()
+}
+
 func CheckLoginUser(context *gin.Context) {
 	token := context.GetHeader(consts.AuthorizationHeaderKey)
 
@@ -30,7 +59,7 @@ func CheckLoginUser(context *gin.Context) {
 		return
 	}
 
-	if dto.Role != "admin" {
+	if dto.Role == "user" {
 		context.AbortWithStatusJSON(http.StatusForbidden, dtos.NewResponseDto("NG", "Forbidden Access", "Please Login With Another Account"))
 		return
 	}
